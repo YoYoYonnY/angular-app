3c2d05ca4bc7a643e10ba0f67b560fe671bfb699
CARAPAXIT\jora <jonne.ransijn@carapaxit.nl>
2017-07-04 12:10:13 +0200

    Refactor example code
    Now generates DOM elements for links in a loop.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My1337App.exe';
+  links = [ { name: "Tour of Heroes", url: "https://angular.io/tutorial" }
+          , { name: "CLI Documentation", url: "https://github.com/angular/angular-cli/wiki" }
+          , { name: "Angular blog", url: "http://angularjs.blogspot.ca/" }
+          , { name: "Find more resources", url: "https://www.google.com" }
+          ]
 }
